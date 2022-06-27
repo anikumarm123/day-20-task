09a35951d2962e6f9af63052e9dd2e9b304257ae
muthukumar <anikumar2825@gmail.com>
2022-06-27 12:35:31 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,87 @@
+// var b ;
+// b = 10 + 20 + "30";
+// console.log(b);
+//output 3030 //
+
+// b = "10"+20+30;
+// console.log(b);
+// output 102030 //
+
+//10 == '10'
+// True //
+
+//10 === '10'
+// false //
+
+// c = 1.5;
+// f=Math.floor(c);
+// console.log(f);
+// // Output 1//
+
+// r = Math.round(c);
+// console.log(r);
+// // Output 2//
+
+// C = Math.ceil(c);
+// console.log(C);
+// // Output 2//
+
+// p = Math.pow(2,3);
+// console.log(p);
+// // Output 8 //
+
+// s = Math.sqrt(81);
+// console.log(s);
+// Output 9 //
+
+// For in and for each difference //
+   
+//1)for in 
+// var fabevy = ["fsd10","fsd9","fed1","fed2"];
+// for (var fsd in fabevy) {
+//     console.log(fsd);
+// }
+// Output index :
+//   0
+//   1
+//   2
+//   3  
+// var obj = {
+//     fsd10:"15",
+//     fsd9:"20",
+//     fed:"30"
+// }
+// for (var object in obj){
+//     console.log(object);
+// }
+// output : keys
+//  fsd10
+// fsd9
+// fed1
+//2)for each 
+// var fabevy = ["fsd10","fsd9","fed1","fed2"];
+// fabevy.forEach(fsd);
+// function fsd(no){
+//     console.log(no);   
+// }
+// output : value
+// fsd10
+// fsd9
+// fed1
+// fed2
+
+// for each method not use in object
+
+// closure
+// function fun(){
+//     var student= "MuthuKumar";
+//     function fun1(){
+//         console.log(student);
+//     }
+//     fun1();
+// }
+// fun()
+
+
+
+  
